d986bddca57dd5eddcaf846e9a1821edf4f82d9a
Johan Svanström <svanis71@gmail.com>
2021-12-03 15:27:43 +0100

    2021 day 3 refactoring

--- 2021/day3/__init__.py
+++ 2021/day3/__init__.py
@@ -1,14 +1,16 @@
 from indata import read_list_of_strings
 
+
 def get_pivot(numbers):
     pivot = []
     for line in numbers:
         for bit, bitval in enumerate(line):
-            if(len(pivot) < bit + 1):
+            if len(pivot) < bit + 1:
                 pivot.append(bitval)
             else:
                 pivot[bit] += bitval
     return pivot
+
 
 def calc_oxygen_generator_rate(bit, numbers):
     if len(numbers) == 1:
@@ -17,12 +19,14 @@ def calc_oxygen_generator_rate(bit, numbers):
     most_common = '1' if pivot[bit].count('1') >= pivot[bit].count('0') else '0'
     return calc_oxygen_generator_rate(bit + 1, [n for n in numbers if n[bit] == most_common])
 
+
 def calc_co2_scrubber_rate(bit, numbers):
     if len(numbers) == 1:
         return numbers[0]
     pivot = get_pivot(numbers)
     least_common = '1' if pivot[bit].count('1') < pivot[bit].count('0') else '0'
     return calc_co2_scrubber_rate(bit + 1, [n for n in numbers if n[bit] == least_common])
+
 
 def part1():
     report = read_list_of_strings('day3')
@@ -42,7 +46,6 @@ def part2():
 def run():
     print(f'Day 3 pt1: {part1()}')
     print(f'Day 3 pt2: {part2()}')
-
 
 # part 1: 1025636
 # part 2: 793873
