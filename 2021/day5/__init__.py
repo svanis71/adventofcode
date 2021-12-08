32b510367549449dca65a56134336f93ca2cab55
Johan Svanström <svanis71@gmail.com>
2021-12-08 10:57:26 +0100

    2021 Day 5 part 2 Wip

--- 2021/day5/__init__.py
+++ 2021/day5/__init__.py
@@ -1,35 +1,60 @@
+import re
 from collections import defaultdict
+
 from indata import read_list_of_strings
 
 
 def part1():
     matrix = defaultdict(int)
     lines = read_list_of_strings('day5')
-    for line in lines:
-        pt1, pt2 = line.replace(' ', '').split('->')
-        x1,y1 = pt1.split(',')
-        x2,y2 = pt2.split(',')
-        if x1 == x2:
-            for y in range(int(min(y1, y2)), int(max(y1, y2))+1):
-                matrix[f'{x1},{y}'] += 1
-        elif y1 == y2:
-            for x in range(int(min(x1, x2)), int(max(x1, x2))+1):
-                matrix[f'{x},{y1}'] += 1
-        else: 
-            print(f'Skipping {x1},{y1} {x2},{y2}')
-    cnt = 0
-    for k,v in matrix.items():
-        if v > 1:
-#            print(f'{k}: {v}')
-            cnt += 1
-    return cnt
 
+    for line in lines:
+        (x1, y1, x2, y2) = [int(p) for p in re.findall(r'(\d+)', line)]
+        if x1 == x2:
+            for y in range(min(y1, y2), max(y1, y2) + 1):
+                matrix[(x1, y)] += 1
+        if y1 == y2:
+            for x in range(min(x1, x2), max(x1, x2) + 1):
+                matrix[(x, y1)] += 1
+    return len([k for k, v in matrix.items() if v >= 2])
 
 
 def part2():
-    pass
+    matrix = defaultdict(int)
+    lines = read_list_of_strings('day5_test')
+
+    for line in lines:
+        (x1, y1, x2, y2) = [int(p) for p in re.findall(r'(\d+)', line)]
+        if x1 == x2:
+            for y in range(min(y1, y2), max(y1, y2) + 1):
+                matrix[(x1, y)] += 1
+        elif y1 == y2:
+            for x in range(min(x1, x2), max(x1, x2) + 1):
+                matrix[(x, y1)] += 1
+        else:
+            print(f'{x1},{y1} -> {x2},{y2} ')
+            if x1 > x2:
+                x1, x2 = x2, x1
+                y1, y2 = y2, y1
+            ydir = -1 if y1 > y2 else 1
+            for yoffset, x in enumerate(range(x1, x2 + 1)):
+                y = y1 + yoffset * ydir
+                print(f'Set {x},{y}')
+                matrix[(x, y)] += 1
+
+    for y in range(10):
+        for x in range(10):
+            if matrix[(x, y)] == 0:
+                print('.', end='')
+            else:
+                print(matrix[(x, y)], end='')
+        print()
+
+    return len([k for k, v in matrix.items() if v >= 2])
 
 
 def run():
-    print(f'Day 5 pt1: {part1()}')
+    # print(f'Day 5 pt1: {part1()}')
     print(f'Day 5 pt2: {part2()}')
+
+# Day 5 pt1: 6267
