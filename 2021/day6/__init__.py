cc17aaf551b27a5a598a4ce7c2445c5068b722e6
Johan Svanström <svanis71@gmail.com>
2021-12-11 21:45:01 +0100

    2021 Day 6 complete day 9 wip

--- 2021/day6/__init__.py
+++ 2021/day6/__init__.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def part1():
     with open('indata/day6.txt') as f:
         lanternfishes = [int(x) for x in f.readline().split(',')]
@@ -16,20 +19,26 @@ def part1():
 
 
 def part2():
-    with open('indata/day6_test.txt') as f:
+    with open('indata/day6.txt') as f:
         lanternfishes = [int(x) for x in f.readline().split(',')]
-    fishtionary = {0: lanternfishes.count(0), 1: lanternfishes.count(1), 2: lanternfishes.count(2), 
-        3: lanternfishes.count(3), 4: lanternfishes.count(4), 5: lanternfishes.count(5), 6: lanternfishes.count(6), 8: 0}
+    fishtionary = {0: lanternfishes.count(0), 1: lanternfishes.count(1), 2: lanternfishes.count(2),
+                   3: lanternfishes.count(3), 4: lanternfishes.count(4), 5: lanternfishes.count(5),
+                   6: lanternfishes.count(6), 7: lanternfishes.count(7), 8: lanternfishes.count(8)}
+    print(f'Initial state: {[(k, fishtionary[k]) for k in sorted(fishtionary.keys())]}')
     for day in range(256):
-        new_day = {}
-        for (age,cnt) in fishtionary.items():
-            if age == 0:
-                new_day[8] = new_day[6] = cnt
-            else:
-                new_day[age - 1] = cnt
-        fishtionary = new_day        
-    return fishtionary           
+        new_day = defaultdict(int)
+        for (age, cnt) in fishtionary.items():
+            if age > 0:
+                new_day[age - 1] += cnt
+            elif cnt > 0:
+                new_day[6] += cnt
+                new_day[8] += cnt
+        fishtionary = new_day
+    return sum(fishtionary.values())
+
 
 def run():
-    # print(f'Day 6 pt1: {part1()}')
+    print(f'Day 6 pt1: {part1()}')
     print(f'Day 6 pt2: {part2()}')
+# 26984457539
+# 716637672781
