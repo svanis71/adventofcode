2cd2bfa78300728ead5a931232af22a783db8a1d
Johan Svanström <svanis71@gmail.com>
2021-12-21 17:54:50 +0100

    2021 Day 13

--- 2021/day13/__init__.py
+++ 2021/day13/__init__.py
@@ -1,17 +1,81 @@
 from indata import read_list_of_strings
 
 
-def part1():
-    pass
+def dump_paper(paper):
+    for row in paper:
+        for col in row:
+            print(col, end='')
+        print()
 
-def part2():
-    pass
+
+def fold_y(from_line, paper):
+    y1, y2 = 0, len(paper) - 1
+    while y1 < y2:
+        for x in range(len(paper[0])):
+            if paper[y2][x] == '#':
+                paper[y1][x] = paper[y2][x]
+                paper[y2][x] = ' '
+        y1, y2 = y1 + 1, y2 - 1
+    paper = [[x for x in row] for y, row in enumerate(paper) if y < from_line]
+
+    return paper
+
+
+def fold_x(from_line, paper):
+    x1, x2 = 0, len(paper[0]) - 1
+    while x1 < x2:
+        for y, row in enumerate(paper):
+            if row[x2] == '#':
+                paper[y][x1] = paper[y][x2]
+                paper[y][x2] = ' '
+        x1, x2 = x1 + 1, x2 - 1
+    paper = [[row[x] for x in range(from_line)] for row in paper]
+    return paper
+
+
+def part1(folding_instructions, paper):
+    dir, from_line = folding_instructions[0]
+    if dir == 'y':
+        paper = fold_y(from_line, paper)
+    else:
+        paper = fold_x(from_line, paper)
+    return sum(row.count('#') for row in paper)
+
+
+def part2(folding_instructions, paper):
+    for (dir, from_line) in folding_instructions:
+        if dir == 'y':
+            paper = fold_y(from_line, paper)
+        else:
+            paper = fold_x(from_line, paper)
+    dump_paper(paper)
+    return sum(row.count('#') for row in paper)
+
+
+def setup():
+    coords = []
+    folding_instructions = []
+    for row in read_list_of_strings('day13'):
+        if len(row) == 0:
+            continue
+        if row[0:4] != 'fold':
+            col, row = row.split(',')
+            coords.append((int(col), int(row)))
+        else:
+            direction, steps = row.split('=')
+            folding_instructions.append((direction[-1], int(steps)))
+    max_x = max(x for (x, y) in coords)
+    max_y = max(y for (x, y) in coords)
+    paper = [[' ' for x in range(max_x + 1)] for y in range(max_y + 1)]
+    for (x, y) in coords:
+        paper[y][x] = '#'
+    return folding_instructions, paper
+
 
 def run():
-    print(f'Day 13 pt1: {part1()}')
-    print(f'Day 13 pt2: {part2()}')
+    folding_instructions, paper = setup()
+    print(f'Day 13 pt1: {part1(folding_instructions, paper)}')
+    print(f'Day 13 pt2: {part2(folding_instructions, paper)}')
 
-
-# Day 13 pt1: 
-# Day 13 pt2: 
-
+# Day 13 pt1: 842
+# Day 13 pt2: BFKRCJZU
--- 2021/main.py
+++ 2021/main.py
@@ -1,54 +1,42 @@
-import os
-import day1
-import day2
-import day3
-import day4
-import day5
-import day6
-import day7
-import day8
-import day9
-import day10
-import day11
-import day12
 import day13
-import day14
-import day15
-import day16
-import day17
-import day18
-import day19
-import day20
-import day21
-import day22
-import day23
-import day24
-import day25
+
+# import day14
+# import day15
+# import day16
+# import day17
+# import day18
+# import day19
+# import day20
+# import day21
+# import day22
+# import day23
+# import day24
+# import day25
 # insert import before (don't remove this line)
 
 if __name__ == '__main__':
-    day1.run()
-    day2.run()
-    day3.run()
-    day4.run()
-    day5.run()
-    day6.run()
-    day7.run()
-    day8.run()
-    day9.run()
-    day10.run()
-    day11.run()
-    day12.run()
+    # day1.run()
+    # day2.run()
+    # day3.run()
+    # day4.run()
+    # day5.run()
+    # day6.run()
+    # day7.run()
+    # day8.run()
+    # day9.run()
+    # day10.run()
+    # day11.run()
+    # day12.run()
     day13.run()
-    day14.run()
-    day15.run()
-    day16.run()
-    day17.run()
-    day18.run()
-    day19.run()
-    day20.run()
-    day21.run()
-    day22.run()
-    day23.run()
-    day24.run()
-    day25.run()
+    # day14.run()
+    # day15.run()
+    # day16.run()
+    # day17.run()
+    # day18.run()
+    # day19.run()
+    # day20.run()
+    # day21.run()
+    # day22.run()
+    # day23.run()
+    # day24.run()
+    # day25.run()
