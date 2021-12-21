81f7149d74eda63c51a32b567fd4ae3a7e7dcdbb
Johan Svanström <svanis71@gmail.com>
2021-12-21 15:06:33 +0100

    2021 prepared packages

---
+++ 2021/day20/__init__.py
@@ -0,0 +1,17 @@
+from indata import read_list_of_strings
+
+
+def part1():
+    pass
+
+def part2():
+    pass
+
+def run():
+    print(f'Day 20 pt1: {part1()}')
+    print(f'Day 20 pt2: {part2()}')
+
+
+# Day 20 pt1: 
+# Day 20 pt2: 
+
--- 2021/main.py
+++ 2021/main.py
@@ -11,6 +11,19 @@ import day9
 import day10
 import day11
 import day12
+import day13
+import day14
+import day15
+import day16
+import day17
+import day18
+import day19
+import day20
+import day21
+import day22
+import day23
+import day24
+import day25
 # insert import before (don't remove this line)
 
 if __name__ == '__main__':
@@ -26,3 +39,16 @@ if __name__ == '__main__':
     day10.run()
     day11.run()
     day12.run()
+    day13.run()
+    day14.run()
+    day15.run()
+    day16.run()
+    day17.run()
+    day18.run()
+    day19.run()
+    day20.run()
+    day21.run()
+    day22.run()
+    day23.run()
+    day24.run()
+    day25.run()
