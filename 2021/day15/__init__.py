a37fa59008f8576944ecb5b84007a73aa3adc1ee
Johan Svanström <svanis71@gmail.com>
2021-12-29 20:47:03 +0100

    2021 Day 15

--- 2021/day15/__init__.py
+++ 2021/day15/__init__.py
@@ -4,57 +4,50 @@ from queue import PriorityQueue
 from indata import read_list_of_strings
 
 
-def find_lowest_risk(grid, start, goal, allow_diagonal_movement=False):
+def find_lowest_risk(grid, multiplier=1):
+    start, size_x, size_y = (0, 0), len(grid[0]), len(grid)
+    x_goal, y_goal = size_x * multiplier - 1, size_y * multiplier - 1
+    risk_levels = defaultdict(int)
+    came_from = defaultdict(tuple)
+
     queue = PriorityQueue()
     queue.put(start, 0)
-    risk_levels = defaultdict(int)
     risk_levels[start] = 0
-    came_from = defaultdict(tuple)
     came_from[start] = None
 
     neighbours = ((0, -1), (0, 1), (-1, 0), (1, 0),)
     while not queue.empty():
         current = queue.get()
-        if current == goal:
+        if current == (x_goal, y_goal):
             break
 
         for nextp_offset in neighbours:
             nextp = (current[0] + nextp_offset[0], current[1] + nextp_offset[1])
-            if nextp[0] > (len(grid) - 1) or nextp[0] < 0 or nextp[1] > (len(grid[len(grid) - 1]) - 1) or nextp[1] < 0:
+            if nextp[0] > x_goal or nextp[0] < 0 or nextp[1] > y_goal or nextp[1] < 0:
                 continue
-            new_cost = risk_levels[current] + grid[nextp[0]][nextp[1]]
-            if nextp not in risk_levels or new_cost < risk_levels[nextp]:
-                risk_levels[nextp] = new_cost
-                prio = new_cost + ((nextp[0] - goal[0]) ** 2) + ((nextp[1] - goal[1]) ** 2)
+            gy, gx = nextp
+            adjusted_risk = (grid[gy % size_y][gx % size_x] + (gx // size_x) + (gy // size_y) - 1) % 9 + 1
+            new_risk_level = risk_levels[current] + adjusted_risk
+            if nextp not in risk_levels or new_risk_level < risk_levels[nextp]:
+                risk_levels[nextp] = new_risk_level
+                prio = new_risk_level + ((nextp[0] - y_goal) ** 2) + ((nextp[1] - x_goal) ** 2)
                 queue.put(nextp, prio)
                 came_from[nextp] = current
-
-    path = [goal]
-    while path[0] != start:
-        path.insert(0, came_from[path[0]])
-    for iy, row in enumerate(grid):
-        for ix, risk_level in enumerate(row):
-            if (iy, ix) in path:
-                print(f'\033[94m\033[1m{risk_level}\033[0m', end='')
-            else:
-                print(f'{risk_level}', end='')
-        print()
-
-    return risk_levels[goal]
+    return risk_levels[(y_goal, x_goal)]
 
 
-def part1():
-    risk_level_map = [[int(risk_level) for risk_level in row] for row in read_list_of_strings('day15')]
-    return find_lowest_risk(risk_level_map, (0, 0), (len(risk_level_map) - 1, len(risk_level_map[0]) - 1))
+def part1(risk_level_map):
+    return find_lowest_risk(risk_level_map)
 
 
-def part2():
-    pass
+def part2(risk_level_map):
+    return find_lowest_risk(risk_level_map, 5)
 
 
 def run():
-    print(f'Day 15 pt1: {part1()}')
-    print(f'Day 15 pt2: {part2()}')
+    risk_level_map = [[int(risk_level) for risk_level in row] for row in read_list_of_strings('day15')]
+    print(f'Day 15 pt1: {part1(risk_level_map)}')
+    print(f'Day 15 pt2: {part2(risk_level_map)}')
 
 # Day 15 pt1: 527
-# Day 15 pt2:
+# Day 15 pt2: 2887
--- 2021/main.py
+++ 2021/main.py
@@ -1,19 +1,34 @@
+import day1
+import day10
+import day11
+import day12
+import day13
+import day14
 import day15
+import day2
+import day3
+import day4
+import day5
+import day6
+import day7
+import day8
+import day9
+
 # insert import before (don't remove this line)
 
 if __name__ == '__main__':
-    # day1.run()
-    # day2.run()
-    # day3.run()
-    # day4.run()
-    # day5.run()
-    # day6.run()
-    # day7.run()
-    # day8.run()
-    # day9.run()
-    # day10.run()
-    # day11.run()
-    # day12.run()
-    # day13.run()
-    # day14.run()
+    day1.run()
+    day2.run()
+    day3.run()
+    day4.run()
+    day5.run()
+    day6.run()
+    day7.run()
+    day8.run()
+    day9.run()
+    day10.run()
+    day11.run()
+    day12.run()
+    day13.run()
+    day14.run()
     day15.run()
