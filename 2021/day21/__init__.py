9d793e383dfc5889727f8dbf7795da4236c7a61b
Johan Svanström <svanis71@gmail.com>
2022-02-02 15:58:44 +0100

    2021 Day 21 part 1 wip

--- 2021/day21/__init__.py
+++ 2021/day21/__init__.py
@@ -1,17 +1,21 @@
-from indata import read_list_of_strings
+from datetime import datetime
 
 
 def part1():
     pass
 
+
 def part2():
     pass
 
-def run():
-    print(f'Day 21 pt1: {part1()}')
-    print(f'Day 21 pt2: {part2()}')
 
+def run():
+    start = datetime.now()
+    print(f'Day 21 pt1: {part1()}')
+    p1end = datetime.now()
+    print(f'Day 21 pt2: {part2()}')
+    p2end = datetime.now()
+    print(f'Total time: {int((p2end - start).total_seconds() * 1000)} ms')
 
 # Day 21 pt1: 
-# Day 21 pt2: 
-
+# Day 21 pt2:
