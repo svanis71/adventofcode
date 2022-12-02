5e802252352433988054024e8564c9ba87d707c6
Johan Svanström <svanis71@gmail.com>
2022-12-02 15:15:17 +0100

    2022 day 1

--- 2022/indata/__init__.py
+++ 2022/indata/__init__.py
@@ -1,22 +1,22 @@
 from os.path import dirname, realpath, join
 
+dir_path = dirname(realpath(__file__))
+
 
 def read_list_of_strings(filename, splitchar=None):
-    dir_path = dirname(realpath(__file__))
-    infile = join(dir_path, f'{filename}.txt')
-    with open(infile, encoding='utf-8') as f:
+    with open(join(dir_path, f'{filename}.txt'), encoding='utf-8') as f:
         content = f.readlines()
     lines = [x.strip() for x in content]
     return lines if not splitchar else [line.split(splitchar) for line in lines]
 
 
 def read_list_of_integers(filename):
-    with open(f'indata/{filename}.txt', encoding='utf-8') as f:
+    with open(join(dir_path, f'{filename}.txt'), encoding='utf-8') as f:
         content = f.readlines()
     return [int(x.strip()) for x in content]
 
 
 def read_csv_integers(filename, splitchar=','):
-    with open(f'indata/{filename}.txt', encoding='utf-8') as f:
+    with open(join(dir_path, f'{filename}.txt'), encoding='utf-8') as f:
         content = f.readline()
     return [int(x.strip()) for x in content.split(splitchar)]
