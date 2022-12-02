c9baba45d7587db67cbfda1399ed1d23df87e301
Johan Svanström <svanis71@gmail.com>
2022-12-02 15:24:08 +0100

    2021 Day 2 visual wip

--- 2021/day2/day2_graphic.py
+++ 2021/day2/day2_graphic.py
@@ -1,5 +1,7 @@
+import sys
+import time
+
 import pygame
-from indata import read_list_of_strings
 
 pygame.init()
 pygame.font.init()
@@ -23,47 +25,49 @@ with open('../indata/day2.txt') as f:
     content = f.readlines()
 lines = [x.strip() for x in content]
 commands = [line.split(' ') for line in lines]
+rotcommands = commands.copy()
 
 boat = pygame.image.load("submarine.png")
 boatrect = boat.get_rect()
-boatrect.top = 200
-boatrect.left = 100
+# boatrect.top = 200
+# boatrect.left = 100
 x, y = 0, 0
-speed = [0, 0]
+speed = [2, 2]
 
-while not done:
-    for command in commands:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-
-            dir, steps = command[0], int(command[1])
-            if dir == 'forward':
-                x += steps
-                speed[0] = steps
-                speed[1] = 0
-            elif dir == 'up':
-                y -= steps
-                speed[0] = 0
-                speed[1] = -steps
-            elif dir == 'down':
-                y += steps
-                speed[0] = 0
-                speed[1] = steps
-
-            boatrect.move(speed)
-            screen.fill(BLACK)
-            screen.blit(boat, boatrect)
-
-            # --- Go ahead and update the screen with what we've drawn.
-            pygame.display.flip()
-        # --- Go ahead and update the screen with what we've drawn.
-        pygame.display.flip()
-        # --- Limit to 60 frames per second
-        clock.tick(60)
-
-while not done:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            done = True
+            sys.exit()
+    # if len(rotcommands) == 0:
+    #     rotcommands = commands.copy()
+    #     x, y = 0, 0
+
+    # command = rotcommands.pop(0)
+    # dir, steps = command[0], int(command[1])
+    # if dir == 'forward':
+    #     x += steps
+    #     speed[0] = steps
+    #     speed[1] = 0
+    # elif dir == 'up':
+    #     y -= steps
+    #     speed[0] = 0
+    #     speed[1] = -steps
+    # elif dir == 'down':
+    #     y += steps
+    #     speed[0] = 0
+    #     speed[1] = steps
+
+    screen.fill(BLACK)
+    # textsurface = myfont.render(f'dir: {dir} steps: {speed}', False, GREEN)
+    # screen.blit(textsurface, (0, 30))
+    textsurface = myfont.render(f'y: {y} x: {x}', False, GREEN)
+    screen.blit(textsurface, (0, 60))
+    screen.blit(boat, boatrect)
+    x += 2
+    y += 2
+    boatrect.left += x
+    boatrect.top += y
+    time.sleep(0.2)
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
     clock.tick(60)
