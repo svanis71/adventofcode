2bdbf28f28ce276223756e6617b26509e8fc92b9
Johan Svanström <svanis71@gmail.com>
2022-12-03 11:26:09 +0100

    Improvements to setup script

--- 2022/setup_day.sh
+++ 2022/setup_day.sh
@@ -8,8 +8,10 @@ echo "from indata import read_list_of_strings
 def part1():
     pass
 
+
 def part2():
     pass
+
 
 def run():
     print(f'Day ${day} pt1: {part1()}')
@@ -21,12 +23,13 @@ def run():
 " > day${day}/__init__.py
 
 
-echo "" > indata/day${day}.txt 
-echo "" > indata/day${day}_test.txt
+[ ! -d "indata" ] && mkdir indata
+touch indata/day${day}.txt
+touch indata/day${day}_test.txt
 
 sed "/^\# insert import before/i import day${day}" main.py > main_tmp.py
 mv main_tmp.py main.py
 
-echo "        day${day}.run()" >> main.py
-echo "        print(f'Time: {perf_counter() - start} seconds')" >> main.py
-echo "        start = perf_counter()" >> main.py
+echo "        day${day}.run()
+              print(f'Time: {perf_counter() - start} seconds')
+              start = perf_counter()" >> main.py
