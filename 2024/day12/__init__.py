2515d97a3a246f3351b499354df790d58d287ca7
Johan Svanström <svanis71@gmail.com>
2024-12-25 20:28:09 +0100

    2024 Day 12 1 & 2

--- 2024/day12/__init__.py
+++ 2024/day12/__init__.py
@@ -1,10 +1,11 @@
+import itertools as it
 from collections import deque
 
 import indata
 from pathfinding import get_adjacent_squares, inbound
 
 
-def get_neighbors(matrix, pos: tuple[int, int]) -> list[tuple[int, int]]:
+def get_neighbors(pos: tuple[int, int]) -> list[tuple[int, int]]:
     y, x = pos[0], pos[1]
     return [(y + offset[0], x + offset[1]) for offset in get_adjacent_squares()]
 
@@ -15,50 +16,56 @@ def is_edge(matrix: list[list[str]], ny: int, nx: int, current_plant: str) -> bo
 
 
 def cost_of_region(matrix, start: tuple[int, int]):
-    que = deque([start])
-    visited = {start}
-    cost = 0
+    plants = deque([start])
+    region = {start}
     perimeter, plant = 0, matrix[start[0]][start[1]]
-    perimeter_nodes = []
-    while len(que) > 0:
-        cy, cx = que.pop()
+    while len(plants) > 0:
+        cy, cx = plants.pop()
         for nxt in ((0, -1), (0, 1), (-1, 0), (1, 0),):
             ny, nx = cy + nxt[0], cx + nxt[1]
             if is_edge(matrix, ny, nx, plant):
                 perimeter += 1
-                perimeter_nodes.append((ny, nx))
-            elif inbound(matrix, ny, nx) and (ny, nx) not in visited:
-                que.append((ny, nx))
-                visited.add((ny, nx))
-    for (y, x) in visited:
+            elif inbound(matrix, ny, nx) and (ny, nx) not in region:
+                plants.append((ny, nx))
+                region.add((ny, nx))
+    for (y, x) in region:
         matrix[y][x] = '-'
-    print(f'A region of {plant} plants with price {len(visited)} * {perimeter} = {perimeter * len(visited)}')
-    cost += (perimeter * len(visited))
-    return cost
+
+    cost = perimeter * len(region)
+    # part 2
+    side_cost = sum(corners(cy, cx, region) for cy, cx in region) * len(region)
+    return cost, side_cost
 
 
-def part1(matrix: list[list[str]]) -> int:
-    cost: int = 0
+def corners(cy, cx, region) -> int:
+    num_corners = 0
+    for oy, ox in it.product([1, -1], repeat=2):
+        row_neighbor, col_neighbor, diagonal_neighbor = (cy + oy, cx), (cy, cx + ox), (cy + oy, cx + ox)
+        if row_neighbor not in region and col_neighbor not in region:
+            num_corners += 1
+        if row_neighbor in region and col_neighbor in region and diagonal_neighbor not in region:
+            num_corners += 1
+    return num_corners
+
+
+def solution(matrix: list[list[str]]) -> tuple[int, int]:
+    costs_of_regions = []
     for iy, row in enumerate(matrix):
         for ix, plant in enumerate(row):
             if plant != '-':
-                cost += cost_of_region(matrix, (iy, ix))
-    return cost
-
-
-def part2(puzzle_input: list[str]) -> int:
-    pass
+                costs_of_regions.append(cost_of_region(matrix, (iy, ix)))
+    return sum(n[0] for n in costs_of_regions), sum(n[1] for n in costs_of_regions)
 
 
 def run():
     puzzle_data = indata.read_list_of_strings('day12', use_testdata=True)
-    matrix = [list(r) for r in puzzle_data]
-    print(f'Day 12 pt1: {part1(matrix)}')
-    print(f'Day 12 pt2: {part2(puzzle_data)}')
+    p1, p2 = solution([list(r) for r in puzzle_data])
+    print(f'Day 12 pt1: {p1}')
+    print(f'Day 12 pt2: {p2}')
 
 
 # Day 12 pt1: 1304764
-# Day 12 pt2: 0
+# Day 12 pt2: 811148
 
 if __name__ == '__main__':
     run()
