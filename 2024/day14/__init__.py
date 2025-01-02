ed43572ea0bef4bae9464405711a13e4b5dbae82
Johan Svanström <svanis71@gmail.com>
2025-01-02 13:39:24 +0100

    2024 Day 14 1 & 2

--- 2024/day14/__init__.py
+++ 2024/day14/__init__.py
@@ -3,14 +3,32 @@ import math
 import indata
 
 
-def part1(robot_data: list[dict], edge_x: int = 101, edge_y: int = 103) -> int:
-    for _ in range(100):
-        for robot in robot_data:
-            move_robot(edge_x, edge_y, robot)
+def read_input(puzzle_data: list[str]) -> tuple:
+    positions: list[tuple[int, int]] = []
+    velocities: list[tuple[int, int]] = []
+    for line in puzzle_data:
+        p_string, v_string = line.split(' ')
+        v = tuple(map(int, v_string[2:].split(',')))
+        p = tuple(map(int, p_string[2:].split(',')))
+        positions.append((p[0], p[1]))
+        velocities.append((v[0], v[1]))
+    return positions, velocities
 
+
+def move_robot(edge_x, edge_y, robot: tuple[int, int], velocity: tuple[int, int]) -> tuple[int, int]:
+    cur_x, cur_y = robot
+    vel_x, vel_y = velocity
+    return (cur_x + vel_x) % edge_x, (cur_y + vel_y) % edge_y
+
+
+def part1(robot_data: list[str], edge_x: int = 101, edge_y: int = 103) -> int:
+    positions, velocities = read_input(robot_data)
+    for _ in range(100):
+        for robot_id, robot in enumerate(positions):
+            positions[robot_id] = move_robot(edge_x, edge_y, robot, velocities[robot_id])
     quads = [0, 0, 0, 0]
-    for robot in robot_data:
-        pos_x, pos_y = robot['pos']
+    for robot in positions:
+        pos_x, pos_y = robot
         if pos_x < edge_x // 2 and pos_y < edge_y // 2:
             quads[0] += 1
         if pos_x > edge_x // 2 and pos_y < edge_y // 2:
@@ -22,50 +40,25 @@ def part1(robot_data: list[dict], edge_x: int = 101, edge_y: int = 103) -> int:
     return math.prod(quads)
 
 
-def move_robot(edge_x, edge_y, robot):
-    cur_x, cur_y = robot["pos"]
-    vel_x, vel_y = robot["velocity"]
-    cur_x += vel_x
-    if cur_x < 0 or cur_x >= edge_x:
-        cur_x = cur_x % edge_x
-    cur_y += vel_y
-    if cur_y < 0 or cur_y >= edge_y:
-        cur_y = cur_y % edge_y
-    robot['pos'] = [cur_x, cur_y]
-
-
-def check_for_tree(robot_data: list[dict]) -> bool:
-    for robot in robot_data:
-        if robot['pos'] == [0, 4]:
-            return True
-    return False
-
-def part2(robot_data: list[dict], edge_x: int = 11, edge_y: int = 7) -> int:
-    found_tree:bool = False
-    turns:int = 0
-    while not found_tree:
+def part2(robot_data: list[str], edge_x: int = 101, edge_y: int = 103) -> int:
+    positions, velocities = read_input(robot_data)
+    turns: int = 0
+    while len(set(positions)) != len(positions):
+        for robot_id, robot in enumerate(positions):
+            positions[robot_id] = move_robot(edge_x, edge_y, robot, velocities[robot_id])
         turns += 1
-        for robot in robot_data:
-            move_robot(edge_x, edge_y, robot)
-        found_tree = check_for_tree(robot_data)
+
     return turns
 
 
 def run():
-    puzzle_data = indata.read_list_of_strings('day14', use_testdata=True)
-    robot_data: list[dict] = []
-    for robot_id, line in enumerate(puzzle_data):
-        p_string, v_string = line.split(' ')
-        p = list(map(int, p_string[2:].split(',')))
-        v = list(map(int, v_string[2:].split(',')))
-        robot_data.append({"robot_id": robot_id, "pos": p, "velocity": v})
-    # 0,4 3,-3
-    print(f'Day 14 pt1: {part1(robot_data, 11, 7)}')
-    print(f'Day 14 pt2: {part2(robot_data)}')
+    puzzle_data = indata.read_list_of_strings('day14', use_testdata=False)
+    print(f'Day 14 pt1: {part1(puzzle_data)}')
+    print(f'Day 14 pt2: {part2(puzzle_data)}')
 
 
 # Day 14 pt1: 229868730
-# Day 14 pt2: 0
+# Day 14 pt2: 7861
 
 if __name__ == '__main__':
     run()
